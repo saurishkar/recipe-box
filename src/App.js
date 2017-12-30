c562760c67471ca1144473805747d924f8d186bf
vinsol <basicallysaurish@gmail.com>
2017-12-30 08:50:13 +0530

    redux logger commented for production

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import ReduxThunk from 'redux-thunk';
-import { logger } from 'redux-logger';
+// import { logger } from 'redux-logger';
 import logo from './logo.svg';
 import './App.css';
 
 import RecipeBoxConnector from './containers/recipe-box-connector';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(ReduxThunk, logger)(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxThunk)(createStore);
 
 class App extends Component {
 	render() {
